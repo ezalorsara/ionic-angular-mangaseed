ca87c9877943f71d9abb939e35ab4e692516bd36
Joed Parin <parinjoed@gmail.com>
2019-12-10 11:41:21 +0800

    feat: add search series functionality

--- src/app/pages/series/series-list/series-list.component.ts
+++ src/app/pages/series/series-list/series-list.component.ts
@@ -9,10 +9,16 @@ import Amplify, { Storage as AwsStorage, API as AwsAPI} from 'aws-amplify';
   styleUrls: ['./series-list.component.scss'],
 })
 export class SeriesListComponent implements OnInit {
-
+  parent = this;
   series = []; 
   loaderItems = [0,1,2,3,4,5,6,7,8,9]; // default 10 items
+  searchSeries = []; 
+  searchLoaderItems = [0,1,2]; // default 10 items
   mSort = "date_asc"; // pre-select order
+  searching:boolean = false;
+  searchTimer = undefined;
+  queryText="";
+
 
   constructor(
     private http:HttpClient
@@ -28,6 +34,38 @@ export class SeriesListComponent implements OnInit {
       this.loadItemAndImage(data.data);
     });
     
+
+  
+  }
+
+  doSearch(event){
+
+    this.queryText = event.target.value.toLowerCase();
+
+    if(this.searchTimer != undefined){
+      clearTimeout(this.searchTimer);
+    }
+    
+    if(this.queryText.length >= 3){
+      this.searching = true;
+      this.searchSeries = [];
+      this.searchLoaderItems = [0,1,2]; // reset loader
+      this.searchTimer = setTimeout(()=>{
+        this.getSeriesSearchList((data, empty)=>{
+          this.loadItemAndImageInSearch(data.data);
+      
+          if(empty){
+            this.searchSeries = [];
+            this.searching = false;
+          }
+
+        }, this.queryText+"&limit=3"); // limit to 3 results
+      }, 1000);
+    }else{
+      this.searchSeries = [];
+      this.searching = false;
+    }
+
   }
 
   doSort(_event){
@@ -74,9 +112,26 @@ export class SeriesListComponent implements OnInit {
       this.loaderItems = [];
       
     }
-    
-  
   }
+
+  loadItemAndImageInSearch(data=[]){
+    if(data.length > 0){
+      AwsStorage.get(data[0].coverImage).then(url=>{
+        data[0].coverImageUrl = url;
+        this.searchSeries.push(data[0]);
+        data.shift();
+        this.searchLoaderItems.shift();
+        this.loadItemAndImageInSearch(data);
+      }).catch(err=>{
+        console.log("Error!");
+        console.log(err);
+      });
+    }else{
+      this.searchLoaderItems = [];
+      this.searching = false;
+    }
+  }
+
 
   async getSeriesList(cb, sortQuery=""){
   
@@ -89,6 +144,26 @@ export class SeriesListComponent implements OnInit {
 
     this.http.get(config.apiUrl + "public/series"+sortQuery, httpOptions).subscribe(data=>{
       cb(data);
+    }, err=>{
+      console.log(err);
+      cb(false);
+    });
+
+  }
+
+  async getSeriesSearchList(cb, searchQuery=""){
+  
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Accept' : 'application/json'
+      })
+    };
+
+    this.http.get(config.apiUrl + "public/search?query="+searchQuery, httpOptions).subscribe(data=>{
+      let result:any = data;
+      let empty:boolean = result.data.length === 0?true:false;
+      cb(data, empty);
     }, err=>{
       console.log(err);
       cb(false);
